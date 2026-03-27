3947aa512015c8e87dada65ae06b1a2043505b5f
Borna Soleimani <B.Soleimani@albynschool.org>
2026-03-27 09:40:15 +0000

    Easter commit

---
+++ area.py
@@ -0,0 +1,3 @@
+height=int(input("what is the height of the rectangle"))
+length=int(input("what is the length of the rectangle"))
+print ("",height,"*",length,"")
---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+
+name=input("what is your name?")
+age=int(input("what is your age"))
+colour=input("what is your favorite colour?")
+print("your name is "+name+"")
+print("your age is ",age,"")
+print("your favorite colour is "+colour+"")
